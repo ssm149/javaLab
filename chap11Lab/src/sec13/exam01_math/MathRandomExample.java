ab5c2e86ddab5ab443230e9376dcedc21a937c18
ssm149 <ssm149@naver.com>
2021-03-31 17:36:39 +0900

    chap11Lap 오늘 날짜 및 년도 체크

---
+++ chap11Lab/src/sec13/exam01_math/MathRandomExample.java
@@ -0,0 +1,16 @@
+package sec13.exam01_math;
+
+public class MathRandomExample {
+
+	public static void main(String[] args) {
+		
+		// 0.0 <= Math.random()  < 1.0 사이의 임의의 실수값
+		 int num = (int)(Math.random()*6) +1;  // 1~6사이의 임의의 정수 생성
+		 System.out.println("주사위 눈 : " + num);
+		 
+		
+		 
+		 
+	}
+
+}
