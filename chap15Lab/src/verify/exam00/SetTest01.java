0f8b172472d6dcb2445607603b6d497b8592c780
ssm149 <ssm149@naver.com>
2021-04-07 13:56:30 +0900

    chap15Lab 예제

--- chap15Lab/src/verify/exam00/SetTest01.java
+++ chap15Lab/src/verify/exam00/SetTest01.java
@@ -1,7 +1,10 @@
 package verify.exam00;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -13,16 +16,26 @@ public class SetTest01 {
 		// TODO Auto-generated method stub
 		int n;
 		Set s = new HashSet();
-		// TreeSet s = new TreeSet();
+		// TreeSet s = new TreeSet();  -- 쉽게 정렬가능
 
 		Random r = new Random();
 
 		// 작성 위치
 		// Set을 이용한 로또
-		while (s.size() != 6)
-			s.add(r.nextInt(45) + 1);
-		System.out.println(s);
 
+		/*
+		 * while (s.size() != 6) s.add(r.nextInt(45) + 1); System.out.println(s);
+		 */
+		while (s.size() != 6) {
+			n = r.nextInt(45) + 1; // 1~45 임의의 정수
+			s.add(n);
+		}
+		// 정렬 : set -> list 변경하여 Collection.sort()를 이용하여 정렬한다.
+		List list = new ArrayList(s);
+		Collections.sort(list);
+		
+		System.out.println(s);
+		System.out.println(list);
 	}
 
 }
