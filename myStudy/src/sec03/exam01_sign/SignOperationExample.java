8ce31fc72feed7f4a1374edce8351e5de2d52eea
ssm149 <ssm149@naver.com>
2021-04-11 08:45:36 +0900

    myStudy

---
+++ myStudy/src/sec03/exam01_sign/SignOperationExample.java
@@ -0,0 +1,22 @@
+package sec03.exam01_sign;
+
+public class SignOperationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = -100;
+		int result1 = +x;
+		int result2 = -x;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		
+		short s = 100;
+		//short result3 = -s;
+		int result3 = -s;
+		
+		System.out.println(result3);
+		
+	}
+
+}
